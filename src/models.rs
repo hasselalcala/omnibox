41fa883a881c2ddd1fc9e526309c3a07425bbd67
Hassel Alcalá <hassel_aurora_13@hotmail.com>
2024-10-21 17:49:21 -0600

    FIX struct

--- src/models.rs
+++ src/models.rs
@@ -1,32 +1,32 @@
 use anyhow::Result;
 use near_sdk::serde_json::{json, Value};
-use near_workspaces::{network::Sandbox, Contract, Worker};
+use near_workspaces::{network::Sandbox, Account, Contract, Worker};
 
 const WASM_PATH: &str = "contract/contract.wasm";
-
 #[derive(Debug)]
 pub struct OmniInfo {
     pub worker: Worker<Sandbox>,
     pub contract: Contract,
+    pub owner: Account,
 }
 
 impl OmniInfo {
     pub async fn new() -> Result<Self> {
         let worker = near_workspaces::sandbox().await?;
-        let wasm = std::fs::read(WASM_PATH)?;
-        let contract = worker.dev_deploy(&wasm).await?;
-
+        let wasm_bytes = std::fs::read(WASM_PATH)?;
+        let contract = worker.dev_deploy(&wasm_bytes).await?;
+        let owner = worker.dev_create_account().await?;
 
         Ok(Self {
             worker,
             contract,
+            owner,
         })
     }
 
     pub async fn call_contract(&self, method: &str, args: Option<Value>) -> Result<Option<Value>> {
-        let owner = self.worker.dev_create_account().await?;
-
-        let result = owner
+        let result = self
+            .owner
             .call(&self.contract.id(), method)
             .args_json(args.unwrap_or(json!({})))
             .transact()
@@ -43,9 +43,8 @@ impl OmniInfo {
     }
 
     pub async fn view_contract(&self, method: &str, args: Option<Value>) -> Result<Value> {
-        let owner = self.worker.dev_create_account().await?;
-
-        let result = owner
+        let result = self
+            .owner
             .view(&self.contract.id(), method)
             .args_json(args.unwrap_or(json!({})))
             .await?;
